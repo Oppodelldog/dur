4c9b887fa9f9672ee329c2b9b91b8474ef094538
Oppodelldog <odog@netcologne.de>
2022-01-01 17:22:28 +0100

    add help output

--- cmd/main.go
+++ cmd/main.go
@@ -12,15 +12,16 @@ func main() {
 	var (
 		options []dur.Option
 		fs      = flag.NewFlagSet("dur", flag.ExitOnError)
-		printer = fs.String("p", "", "-p=h -p=n (h - human readable, n - nanoseconds)")
-		err     = fs.Parse(os.Args[1:])
+		printer = fs.String("p", "", "prints a line for each calculation that is performed.\nOutput options:\n  h - human readable\n  n - nanoseconds\nexample: -p=h")
 	)
-
-	if err != nil {
-		panic(err)
-	}
+	fs.Usage = usage(fs)
+	var err = fs.Parse(os.Args[1:])
 
 	input := strings.Join(os.Args[len(os.Args)-fs.NArg():], "")
+
+	if len(input) == 0 || err != nil {
+		fs.Usage()
+	}
 
 	switch *printer {
 	case "h":
@@ -30,4 +31,17 @@ func main() {
 	}
 
 	fmt.Println(dur.NewCalculator(input, options...).Calculate())
+}
+
+func usage(fs *flag.FlagSet) func() {
+	return func() {
+		fmt.Println("dur - duration calculation")
+		fmt.Println("usage: dur [-p] OPERAND [OPERATION OPERAND ...]")
+		fmt.Println()
+		fmt.Println("example: dur 40h + 2h")
+		fmt.Println(" output: 42h0m0s")
+		fmt.Println()
+		fmt.Println("Options:")
+		fs.PrintDefaults()
+	}
 }
