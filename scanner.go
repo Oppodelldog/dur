33b56d2333f00f63f97c04000b0a8e770abe7031
Oppodelldog <odog@netcologne.de>
2022-04-23 12:48:57 +0200

    add parentheses tokens

--- scanner.go
+++ scanner.go
@@ -6,10 +6,12 @@ import (
 )
 
 const (
-	TypeEOF        TokenType = "EOF"
-	TypeWhitespace TokenType = "WHITESPACE"
-	TypePlus       TokenType = "PLUS"
-	TypeMinus      TokenType = "MINUS"
+	TypeEOF         TokenType = "EOF"
+	TypeWhitespace  TokenType = "WHITESPACE"
+	TypePlus        TokenType = "PLUS"
+	TypeMinus       TokenType = "MINUS"
+	TypeParenOpen   TokenType = "PAREN_OPEN"
+	TypeParentClose TokenType = "PAREN_CLOSE"
 
 	TypeValue = "VALUE"
 
@@ -17,9 +19,11 @@ const (
 )
 
 const (
-	minus = '-'
-	plus  = '+'
-	space = ' '
+	minus      = '-'
+	plus       = '+'
+	space      = ' '
+	parenOpen  = '('
+	parenClose = ')'
 )
 
 type TokenType string
@@ -109,6 +113,14 @@ func (s *Scanner) nextToken() Token {
 		s.nextChar()
 	case ch == plus:
 		tok = Token{Type: TypePlus}
+
+		s.nextChar()
+	case ch == parenOpen:
+		tok = Token{Type: TypeParenOpen}
+
+		s.nextChar()
+	case ch == parenClose:
+		tok = Token{Type: TypeParentClose}
 
 		s.nextChar()
 	case isDigit(ch):
